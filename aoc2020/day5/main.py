3a44e1da73a9b2b1d67b8f8ea9b3959404decc73
Rémi Berson <remi@cliqz.com>
2020-12-05 14:14:47 +0100

    Shorter

--- aoc2020/day5/main.py
+++ aoc2020/day5/main.py
@@ -1,17 +1,7 @@
 def main():
     with open("./input1.txt") as inputs:
         ids = [
-            int(
-                line.translate(
-                    {
-                        ord("B"): ord("1"),
-                        ord("F"): ord("0"),
-                        ord("R"): ord("1"),
-                        ord("L"): ord("0"),
-                    }
-                ),
-                2,
-            )
+            int(line.translate(str.maketrans("BFRL", "1010")), 2)
             for line in inputs
         ]
         min_id, max_id, sum_id = min(ids), max(ids), sum(ids)
