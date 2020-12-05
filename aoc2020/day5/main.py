fca5669c69bc0068e2d3e30cfb8e940d376f8db3
Rémi Berson <remi@cliqz.com>
2020-12-05 14:04:55 +0100

    Convert to int64 directly

--- aoc2020/day5/main.py
+++ aoc2020/day5/main.py
@@ -1,12 +1,21 @@
 def main():
     with open("./input1.txt") as inputs:
         ids = [
-            8 * sum(2 ** i for i, l in enumerate(line[0:7][::-1]) if l == "B")
-            + sum(2 ** i for i, l in enumerate(line[7:10][::-1]) if l == "R")
+            int(
+                line.translate(
+                    {
+                        ord("B"): ord("1"),
+                        ord("F"): ord("0"),
+                        ord("R"): ord("1"),
+                        ord("L"): ord("0"),
+                        ord("\n"): None,
+                    }
+                ),
+                2,
+            )
             for line in inputs
         ]
         min_id, max_id, sum_id = min(ids), max(ids), sum(ids)
-
         print("Part 1:", max_id)
         print("Part 2:", sum(range(min_id, max_id + 1)) - sum_id)
 
