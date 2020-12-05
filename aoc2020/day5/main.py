865ac3a788e2921cb90042277056899d6e5c77a6
Rémi Berson <remi@cliqz.com>
2020-12-05 11:55:01 +0100

    Shorter

--- aoc2020/day5/main.py
+++ aoc2020/day5/main.py
@@ -10,10 +10,10 @@ def get_seat_id(bp):
 def main():
     with open("./input1.txt") as inputs:
         ids = [get_seat_id(line.strip()) for line in inputs]
-        min_id = min(ids)
-        max_id = max(ids)
+        min_id, max_id, sum_id = min(ids), max(ids), sum(ids)
+
         print("Part 1:", max_id)
-        print("Part 2:", sum(range(min_id, max_id + 1)) - sum(ids))
+        print("Part 2:", sum(range(min_id, max_id + 1)) - sum_id)
 
 
 if __name__ == "__main__":
