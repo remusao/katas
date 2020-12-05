3d5f02482ddbb4267f88d137031b1ae6a64611e2
Rémi Berson <remi@cliqz.com>
2020-12-05 12:05:24 +0100

    Shorter

--- aoc2020/day5/main.py
+++ aoc2020/day5/main.py
@@ -3,7 +3,7 @@ def main():
         ids = [
             8 * sum(2 ** i for i, l in enumerate(line[0:7][::-1]) if l == "B")
             + sum(2 ** i for i, l in enumerate(line[7:10][::-1]) if l == "R")
-            for line in inputs.readlines()
+            for line in inputs
         ]
         min_id, max_id, sum_id = min(ids), max(ids), sum(ids)
 
