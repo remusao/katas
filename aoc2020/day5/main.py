d364de43dd87ba9538b2e58f2d5570c031568d6a
Rémi Berson <remi@cliqz.com>
2020-12-05 11:45:07 +0100

    No need for sets

--- aoc2020/day5/main.py
+++ aoc2020/day5/main.py
@@ -21,11 +21,11 @@ def get_seat_id(bp):
 
 def main():
     with open("./input1.txt") as inputs:
-        ids = {get_seat_id(line.strip()) for line in inputs}
+        ids = [get_seat_id(line.strip()) for line in inputs]
         min_id = min(ids)
         max_id = max(ids)
         print("Part 1:", max_id)
-        print("Part 2:", set(range(min_id, max_id)) - ids)
+        print("Part 2:", sum(range(min_id, max_id + 1)) - sum(ids))
 
 
 if __name__ == "__main__":
