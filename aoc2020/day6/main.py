c233ba9b31783a1042dd3f5bd1db3e28bba6cc03
Rémi Berson <remi@cliqz.com>
2020-12-06 09:23:06 +0100

    AOC 2020: day 6

---
+++ aoc2020/day6/main.py
@@ -0,0 +1,26 @@
+
+from collections import Counter
+import re
+
+def solve1():
+    with open('./input1.txt') as inputs:
+        res = 0
+        for group in inputs.read().strip().split('\n\n'):
+            res += len(set(re.compile(r'\s+').sub('', group)))
+        print(res)
+
+
+def solve2():
+    with open('./input1.txt') as inputs:
+        res = 0
+        for group in inputs.read().strip().split('\n\n'):
+            persons = len(group.split('\n'))
+            count = Counter(re.compile(r'\s+').sub('', group))
+            for c, cnt in count.items():
+                if cnt == persons:
+                    res += 1
+        print(res)
+
+
+if __name__ == "__main__":
+    solve2()
