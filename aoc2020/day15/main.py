af3e6df8fa131b176c7430885a683d490d079227
Rémi Berson <remi@cliqz.com>
2020-12-15 14:22:03 +0100

    Faster with array

--- aoc2020/day15/main.py
+++ aoc2020/day15/main.py
@@ -1,30 +1,23 @@
+#!/usr/bin/env python
+
+import array
+
+
 def run(numbers, stop):
-    last_seen = {}
+    last_seen = array.array("I", [0] * stop)
     for i, n in enumerate(numbers[:-1]):
         last_seen[n] = i + 1
 
-    # Start with last number in the sequence
     i = len(numbers)
     n0 = numbers[-1]
-    l = last_seen.get(n0, None)
-    last_seen[n0] = i
+    l, last_seen[n0] = last_seen[n0], i
 
-    # Start computing numbers of the sequence
-    while True:
-        if l is None:
-            n0 = 0
-        else:
-            n0 = i - l
-
+    while i != stop:
+        n0 = 0 if l == 0 else i - l
         i += 1
-        l = last_seen.get(n0)
-        last_seen[n0] = i
+        l, last_seen[n0] = last_seen[n0], i
 
-        if i == stop:
-            # print(len(last_seen))
-            # print(min(last_seen))
-            # print(max(last_seen))
-            return n0
+    return n0
 
 
 def solve1(numbers):
@@ -36,9 +29,16 @@ def solve2(numbers):
 
 
 if __name__ == "__main__":
+    import time
+
     inputs = "0,3,6"
     inputs = "9,12,1,4,17,0,18"
     numbers = list(map(int, inputs.split(",")))
 
     print("Part 1:", solve1(numbers))
     print("Part 2:", solve2(numbers))
+
+    t0 = time.time()
+    solve2(numbers)
+    t1 = time.time()
+    print(t1 - t0)
